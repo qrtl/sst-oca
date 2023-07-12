659a635272b06d647e691f3baa43d85631c1ded1
Aungkokolin1997 <lin@quartile.co>
2023-07-12 12:54:10 +0630

    [ADD] base_group_backend

---
+++ base_group_backend/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Pierre Verkest <pierreverkest84@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Base Group Backend",
+    "version": "11.0.1.0.0",
+    "category": "Tools",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "website": "https://www.quartile.co",
+    "depends": ["base", "mail"],
+    "data": [
+        "data/res_groups.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
